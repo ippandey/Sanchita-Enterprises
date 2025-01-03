ed2b08e36dec0aa4b54dbff10dd7bad27bf6ec7a
Priyanshu <priyanshu.p2025@gmail.com>
2025-01-03 19:40:55 +0530

    Your descriptive commit message

---
+++ Frontend/src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from "react";
+
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div>This is a sample page. </div>
+      <p>This is a sample page.</p>
+    </>
+  );
+}
+
+export default App;
