ca52e54e3de1e979095a1d45cb21b736113169bb
Priyanshu <priyanshu.p2025@gmail.com>
2025-01-09 12:08:19 +0530

    Ticker added

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,16 +1,23 @@
-import Carousal from "./Components/Carousal/Carousal";
+import Carousel from "./Components/Carousel/Carousel";
 import Navbar from "./Components/Navbar/Navbar";
+import TextTicker from "./Components/TextTicker/TextTicker";
 
 function App() {
   return (
     <>
+      {/* Navbar */}
       <section className="overflow-x-hidden ">
         <Navbar />
       </section>
 
+      {/* Carousel */}
       <section>
-        <Carousal />
+        <Carousel />
       </section>
+
+      {/* Text Ticker/Marquee */}
+
+      <TextTicker />
     </>
   );
 }
